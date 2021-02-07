3e6ce219c282ba9df0821b6689bc4d09b34c49c2
SeongChan Lee <foriequal@gmail.com>
2021-02-07 20:48:06 +0900

    Move some functions out of pod_graceful_drain file
    also invert conditions to avoid ! operator

--- internal/pkg/core/pod_state.go
+++ internal/pkg/core/pod_state.go
@@ -1,9 +1,12 @@
 package core
 
 import (
+	"context"
 	"github.com/pkg/errors"
 	corev1 "k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/types"
 	kubepod "k8s.io/kubernetes/pkg/api/v1/pod"
+	"sigs.k8s.io/controller-runtime/pkg/client"
 	"time"
 )
 
@@ -66,4 +69,25 @@ func (i *PodDeletionDelayInfo) GetRemainingTime(now time.Time) time.Duration {
 	} else {
 		return i.DeleteAtUTC.Sub(nowUTC)
 	}
+}
+
+func IsPodInDrainingNode(ctx context.Context, client client.Client, pod *corev1.Pod) (bool, error) {
+	nodeName := pod.Spec.NodeName
+	var node corev1.Node
+	if err := client.Get(ctx, types.NamespacedName{Name: nodeName}, &node); err != nil {
+		return false, errors.Wrapf(err, "cannot get node %v", nodeName)
+	}
+
+	// Node is about to be drained.
+	// `kubectl drain` will fail and stop if it meets the first pod that cannot be deleted.
+	// It'll cordon a node before draining, so we detect it, and try not to deny the admission.
+	if node.Spec.Unschedulable {
+		return true, nil
+	}
+	for _, taint := range node.Spec.Taints {
+		if taint.Key == corev1.TaintNodeUnschedulable {
+			return true, nil
+		}
+	}
+	return false, nil
 }
